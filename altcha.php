5ae0ed04d67fff2a747105de5d05c2407f202531
Daniel Regeci <536331+ovx@users.noreply.github.com>
2025-08-19 10:10:28 +0200

    1.25.0

--- altcha.php
+++ altcha.php
@@ -7,8 +7,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * Description: ALTCHA is a free, open-source CAPTCHA alternative that offers robust protection without using cookies, ensuring full GDPR compliance by design. It also provides invisible anti-spam and anti-bot protection through ALTCHA's API.
  * Author: Altcha.org
  * Author URI: https://altcha.org
- * Version: 1.24.0
- * Stable tag: 1.24.0
+ * Version: 1.25.0
+ * Stable tag: 1.25.0
  * Requires at least: 5.0
  * Requires PHP: 7.3
  * Tested up to: 6.6
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html  
  */
 
-define('ALTCHA_VERSION', '1.24.0');
+define('ALTCHA_VERSION', '1.25.0');
 define('ALTCHA_WEBSITE', 'https://altcha.org/');
 define('ALTCHA_WIDGET_VERSION', '2.0.2');
 
